153f666e7df08153be3205d096748f0bfee97cd0
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2015-01-11 23:48:10 +0000

    Console
    git-svn-id: https://darkengines.com/svn/darkengines/ArtificialNeuralNetworkDataFeeder@608 302f0312-3c5d-0d4b-b714-136c201f073e

---
+++ ArtificialNeuralNetworkDataFeeder.Console/DatumClassMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtificialNeuralNetworkDataFeeder.Core;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace ArtificialNeuralNetworkDataFeeder.Console {
+	public class DatumClassMap : CsvClassMap<Datum> {
+		public DatumClassMap() {
+			Map(datum => datum.Date).Index(0).ConvertUsing<DateTime>(row => {
+				return DateTime.Parse(string.Format("{0} {1}", row.GetField(0), row.GetField(1)));
+			});
+			Map(datum => datum.Open).Index(2);
+			Map(datum => datum.High).Index(3);
+			Map(datum => datum.Low).Index(4);
+			Map(datum => datum.Close).Index(5);
+			Map(datum => datum.Volume).Index(6);
+		}
+	}
+}
---
+++ ArtificialNeuralNetworkDataFeeder.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ArtificialNeuralNetworkDataFeeder.Core;
+using ArtificialNeuralNetworkDataFeeder.DataIndicator;
+using ArtificialNeuralNetworkDataFeeder.DataPickers;
+using CsvHelper;
+
+namespace ArtificialNeuralNetworkDataFeeder.Console {
+	class Program {
+		static void Main(string[] args) {
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			var path = args[0];
+			Datum[] data = null;
+			using(var stream = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+				using (var reader = new StreamReader(stream)) {
+					var csv = new CsvReader(reader);
+					csv.Configuration.RegisterClassMap<DatumClassMap>();
+					data = csv.GetRecords<Datum>().ToArray();
+				}
+			}
+			var dataProvider = new DataProvider();
+			dataProvider.InputDataPickers.Add(new MovingAverageDataPicker() { Index = 0, Indicator = new MovingAverageIndicator() });
+			var set = dataProvider.Build(data);
+		}
+	}
+}
