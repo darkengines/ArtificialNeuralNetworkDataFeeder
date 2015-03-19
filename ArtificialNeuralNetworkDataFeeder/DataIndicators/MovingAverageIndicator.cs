e80ed5b020ebe34961b4a51436ea32598bff14e2
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2015-03-19 00:25:54 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/ArtificialNeuralNetworkDataFeeder@631 302f0312-3c5d-0d4b-b714-136c201f073e

--- ArtificialNeuralNetworkDataFeeder/DataIndicators/MovingAverageIndicator.cs
+++ ArtificialNeuralNetworkDataFeeder/DataIndicators/MovingAverageIndicator.cs
@@ -1,20 +1,20 @@
-﻿using ArtificialNeuralNetworkDataFeeder.Core;
+﻿using System;
+using ArtificialNeuralNetworkDataFeeder.Core;
 
 namespace ArtificialNeuralNetworkDataFeeder.DataIndicators {
-	public class MovingAverageIndicator : DataIndicator {
+	public class MovingAverageIndicator : IDataIndicator {
 		public int Period { get; set; }
-		public override double Process(double[] data, int index)
-		{
+		public int InputCount { get { return Period; } }
+		public int OuputCount { get { return Period; } }
+		public double Process(double[] data, int index) {
 			index = index + 1;
 			var limit = index - Period;
 			var sum = 0d;
 			while (index-- > limit) sum += data[index];
 			return sum / Period;
 		}
-
-		public override int InputCount
-		{
-			get { return Period; }
+		public double[] Process(double[] data, int index, int count) {
+			
 		}
 	}
 }
